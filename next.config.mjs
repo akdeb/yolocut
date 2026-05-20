c23d95fd3aca6487c2e35467a9d434833a60187f
akdeb <deb2@illinois.edu>
2026-05-20 17:28:15 +0530

    lets try to fix again ;)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverExternalPackages: [
+    "@remotion/renderer",
+    "@remotion/compositor-darwin-arm64",
+    "@remotion/compositor-darwin-x64",
+    "@remotion/compositor-linux-x64-gnu",
+    "@remotion/compositor-linux-x64-musl",
+    "@remotion/compositor-linux-arm64-gnu",
+    "@remotion/compositor-linux-arm64-musl",
+    "@remotion/compositor-win32-x64-msvc",
+  ],
+};
 
 export default nextConfig;
